6cbc2424094141c93781075c4073c2453f5ccc68
Prem Sai K <premsai224k@gmail.com>
2024-08-14 16:56:07 +0530

    Added required files
    this is my first hackathon files

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function redirectToPage() {
+    window.location.href = 'lowcarb.html'; // Redirect to the low carb diet page
+}
+function redirect(){
+    window.location.href = 'chicken_nutri.html';
+}
+var swiper = new Swiper('.brand-slider', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
